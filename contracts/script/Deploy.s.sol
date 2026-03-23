3e54af865bb05d6c48a920275e878d893a98315c
caszete <andrew@concorde.studio>
2026-03-23 11:44:10 -0400

    small forge fixes

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {ClaimVerifier} from "../src/ClaimVerifier.sol";
 
 contract Deploy is Script {
+    uint64 internal constant DEFAULT_CHALLENGE_PERIOD = 120;
+    uint256 internal constant DEFAULT_MIN_BOND = 0.01 ether;
+
     function run() external {
         vm.startBroadcast();
-        ClaimVerifier verifier = new ClaimVerifier();
+        ClaimVerifier verifier = new ClaimVerifier(
+            DEFAULT_CHALLENGE_PERIOD,
+            DEFAULT_MIN_BOND
+        );
         vm.stopBroadcast();
 
         console.log("ClaimVerifier deployed at:", address(verifier));
